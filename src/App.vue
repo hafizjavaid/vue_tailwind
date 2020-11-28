47e28c53ce4046b3538264a3fead1caba8fa80f2
hafizjavaid <hafizjaidi786@gmail.com>
2020-11-28 22:30:44 +0500

    First

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1 class="text-base h-16 w-16 text-red-400 font-bold">Hello</h1>
   </div>
 </template>
-
+<script>
+export default {};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import url("./assets/tailwind.css");
 </style>
